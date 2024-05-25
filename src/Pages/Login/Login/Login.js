b04a7502be1dcb6e83b9645d19002bdf6315aec8
faysalahmmed1 <faysalahmmedcse@gmail.com>
2024-05-25 15:41:33 +0600

    my fifth commit

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,23 +1,24 @@
-
-import { useRef } from 'react';
 import './Login.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { useState } from 'react';
 
 
 
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const [
-        createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate();
+    const navigateee = useNavigate();
+
+    const [SignInWithGoogle, users] = useSignInWithGoogle(auth);
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+   
     if (error) {
         return (
             <div>
@@ -29,26 +30,24 @@ const Login = () => {
         return <p>Loading...</p>;
     }
     if (user) {
-       Navigator('/home');
+        return navigate('/');
+        
+        
     }
+    if (users) {
 
-    const handleSubmite = (e) => {
-        e.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        createUserWithEmailAndPassword(email, password);
-
+        return navigateee('/');
+        
     }
-
 
     return (
         <div className='login-container'>
             <h1>Login</h1>
             <div>
-                <Form onSubmit={handleSubmite} >
+                <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                        <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" value={email} placeholder="Enter email" required />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -56,16 +55,16 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                        <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" value={password} placeholder="Password" required />
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button onClick={() => signInWithEmailAndPassword(email, password)} variant="primary" type="submit">
                         Submit
                     </Button>
                 </Form>
                 <p className='login-link'>Create Now Account?  <span><Link style={{ textDecoration: 'none', color: 'red', fontSize: '20px', marginTop: '20px' }} to={'/signup'}>Signup</Link></span></p>
             </div>
             <div className='button-container'>
-                <button className='google-button'>SignIn With Google</button>
+                <button onClick={() => SignInWithGoogle()} className='google-button'>SignIn With Google</button>
             </div>
         </div>
     );
--- src/Pages/Login/Signup/Signup.js
+++ src/Pages/Login/Signup/Signup.js
@@ -1,53 +1,101 @@
-import React, { useRef } from 'react';
+import { useState } from 'react';
 import './Signup.css';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 
 
 const Signup = () => {
-    const nameRef = useRef('')
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const handleSubmite = (e) => {
-        e.preventDefault();
-        const name = nameRef.current.value;
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        console.log(email, password,name);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState();
 
-        
+    const navigate = useNavigate();
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
+            </div>
+        );
     }
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    if (user) {
+        return navigate('/')
+    }
+
+    const handleEmail = (event) => {
+        setEmail(event.target.value);
+
+    }
+
+    const handlepassword = (event) => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPassword = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+    const handleSubmite = (event) => {
+        event.preventDefault();
+        createUserWithEmailAndPassword(email, password, confirmPassword);
+    }
+
+
 
     return (
         <div className='signUp-container'>
             <h1>Sign Up</h1>
+
             <Form onSubmit={handleSubmite}>
                 <Form.Group className="mb-3" controlId="formBasicName">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={nameRef} type="name" placeholder="Your name" />
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="name" placeholder="Your name" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                    <Form.Control onChange={handleEmail} type="email" placeholder="Enter email" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                    <Form.Control onChange={handlepassword} type="password" placeholder="Password"  required/>
                 </Form.Group>
-
-                {/* <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="Confirmpassword" placeholder="ConfirmPassword" />
-                </Form.Group> */}
+                    <Form.Control onChange={handleConfirmPassword} type="ConfirmPassword" placeholder="ConfirmPassword" required/>
+                </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
+
             <p className='signUp-link'>Already You have an Account?  <span><Link style={{ textDecoration: 'none', color: 'red', fontSize: '20px', marginTop: '20px' }} to={'/login'}>login</Link></span></p>
         </div>
     );
 };
 
 export default Signup;
+
+
+
+
+
+
+
+
+
+
+
+
+
